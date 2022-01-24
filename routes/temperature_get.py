b981f10bb74c33ccce9d1f0cf5891c1442756707
Teddy Ordoñez <teddyonu@hotmail.com>
2022-01-24 20:23:20 -0300

    changed env vars to .env file

--- routes/temperature_get.py
+++ routes/temperature_get.py
@@ -2,14 +2,16 @@ from flask import Blueprint, request
 from .cache import cache
 import os
 import requests
+from dotenv import  load_dotenv
+load_dotenv()
 
 temperature_get = Blueprint('temperature_get', __name__, template_folder= '../templates')
 
 @temperature_get.route('/temperature/<string:city_name>')
 def get_weather(city_name):
     city_name = city_name.lower()       # Lower string to have more control withou worring how the user wrote it
-    cache_ttl = int(os.environ.get('CACHE_TTL'))     # Getting cache_ttl from environment variables, converting to int because it return string
-    default_max_number = int(os.environ.get('DEFAULT_MAX_NUMBER'))  # Getting default max number from environment variables, converting to int because it return string
+    cache_ttl = int(os.getenv('CACHE_TTL'))     # Getting cache_ttl from environment variables, converting to int because it return string
+    default_max_number = int(os.getenv('DEFAULT_MAX_NUMBER'))  # Getting default max number from environment variables, converting to int because it return string
     
     keys = cache.get('id')              # Getting the ids from cache to verify if it was queried before
     if keys == None:                    # If there are no ids in cache
@@ -18,7 +20,7 @@ def get_weather(city_name):
     data = cache.get(city_name)         # Looking for data stored in cache
     if data == None:                    # If data is not stored, do the GET request
 
-        api_key = os.environ.get('OPEN_WEATHER_API_KEY')    # Getting API key from environment variables
+        api_key = os.getenv('OPEN_WEATHER_API_KEY')    # Getting API key from environment variables
         url= f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric'       # URL for making get request
 
         resp= requests.get(url).json()  # Getting response in JSON format
